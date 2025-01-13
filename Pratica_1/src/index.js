2c0396852376c2f8795291b00949824aa82780b3
Pedro Henrique Gomes Lima <pedrohenriquegomeslima1@gmail.com>
2025-01-13 14:51:30 -0300

    works with props

--- Pratica_1/src/index.js
+++ Pratica_1/src/index.js
@@ -1,29 +1,69 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
 import { StrictMode } from "react";
+import "./index.css";
 function App() {
   return (
-    <div>
-      <h1>Hello React!!!!!!!!</h1>
-      <Pizza />
+    <div className="container">
+      <Header></Header>
+      <Menu></Menu>
+      <Footer></Footer>
     </div>
   );
 }
-function Pizza() {
+function Pizza(props) {
   return (
-    <div>
-    <img src="pizzas/spinaci.jpg" alt='pizza de espinafri'/>
-    <h2>Pizza Spinaci</h2>
-    <p>Tomato, mozarella, Spinach, and ricotta cheese</p>
+    <div class='pizza'>
+      <img src={props.photoName} alt={props.name} />
+      <div>
+      <h3>{props.name}</h3>
+      <p>{props.ingredients}</p>
+      <span>{props.price + 1}</span>
+      </div>
     </div>
-  )
+  );
+}
+function Header() {
+  //const style = {color : "red", fontSize: "42px", textTransform: "uppercase"};
+  const style = {};
+  return (
+    <header className="header">
+      <h1 style={style}>Pizza do Pedrão</h1>{" "}
+    </header>
+  );
 }
 
-function Menu(){
+function Menu() {
+  return (
+    <main className="menu">
+      <h2> Our Menu</h2>
+      <Pizza
+        name="Pizza Spinaci"
+        ingredients="Tomato, mozarella, Spinach, and ricotta cheese"
+        photoName="pizzas/spinaci.jpg"
+        price={10}
+      />
+      <Pizza
+        name="Pizza Funghi"
+        ingredients="Tomato, mozarella, mushrooms, and onion"
+        photoName="pizzas/funghi.jpg"
+        price={15}
+      />
 
+    </main>
+  );
 }
-function Footer(){
-
+function Footer() {
+  const hour = new Date().getHours();
+  const openHour = 12;
+  const closeHour = 22;
+  const isOpen = hour >= openHour && hour <= closeHour;
+  console.log(isOpen);
+  //if (hour >= openHour && hour <= closeHour) alert("We're currently open");
+  //else alert("We're currently closed");
+  return (
+    <footer className='footer'>{new Date().toLocaleTimeString()}. We're currently open</footer>
+  );
 }
 
 const domNode = document.getElementById("root");
